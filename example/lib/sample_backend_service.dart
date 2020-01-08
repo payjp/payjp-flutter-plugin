2567fff0d2dc9c56b5bf24e34dad179891293fa0
yatatsu <yatatsukitagawa@gmail.com>
2020-01-08 11:31:07 +0900

    Update copyrights.

--- example/lib/sample_backend_service.dart
+++ example/lib/sample_backend_service.dart
@@ -1,9 +1,32 @@
+/*
+ *
+ * Copyright (c) 2020 PAY, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:payjp_flutter/card_token.dart';
 import 'package:http/http.dart' as http;
+import 'package:payjp_flutter/card_token.dart';
 
 ///
 /// TODO: REPLACE WITH YOUR ENDPOINT URL
@@ -23,7 +46,7 @@ class ApiException implements Exception {
 
 Future<void> saveCard(Token token) async {
   try {
-    final formData = { "card": token.id };
+    final formData = {"card": token.id};
     var response = await http.post(backendUrl, body: formData);
     final body = json.decode(response.body);
     print(body);
--- example/lib/widgets/alert_dialog.dart
+++ example/lib/widgets/alert_dialog.dart
@@ -1,14 +1,36 @@
+/*
+ *
+ * Copyright (c) 2020 PAY, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-Future<void> showAlertDialog({
-  BuildContext context,
-  String title,
-  String message,
-  String button = "OK"
-}) {
+Future<void> showAlertDialog(
+    {BuildContext context,
+    String title,
+    String message,
+    String button = "OK"}) {
   final titleText = Text(title);
   final content = SingleChildScrollView(
     child: Text(message),
@@ -16,30 +38,30 @@ Future<void> showAlertDialog({
   final buttonText = Text(button);
   if (Platform.isIOS) {
     return showCupertinoDialog<void>(
-      context: context,
-      builder: (context) => CupertinoAlertDialog(
-        title: titleText,
-        content: content,
-        actions: <Widget>[
-          CupertinoDialogAction(
-            child: buttonText,
-            onPressed: () => Navigator.of(context).pop(),
-          )
-        ],
-      ));
+        context: context,
+        builder: (context) => CupertinoAlertDialog(
+              title: titleText,
+              content: content,
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  child: buttonText,
+                  onPressed: () => Navigator.of(context).pop(),
+                )
+              ],
+            ));
   } else {
     return showDialog<void>(
-      context: context,
-      barrierDismissible: true,
-      builder: (context) => AlertDialog(
-        title: titleText,
-        content: content,
-        actions: <Widget>[
-          FlatButton(
-            child: buttonText,
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-        ],
-      ));
+        context: context,
+        barrierDismissible: true,
+        builder: (context) => AlertDialog(
+              title: titleText,
+              content: content,
+              actions: <Widget>[
+                FlatButton(
+                  child: buttonText,
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            ));
   }
 }
