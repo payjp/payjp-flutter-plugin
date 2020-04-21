9c0f81a8b0bd9c259f6d383fefffae66dda3f95f
yatatsu <yatatsukitagawa@gmail.com>
2020-04-21 15:36:29 +0900

    Add 3DS redirect configuration.

---
+++ lib/src/three_d_secure.dart
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020 PAY, Inc.
+ *
+ * Use of this source code is governed by a MIT License that can by found in the LICENSE file.
+ */
+import 'package:meta/meta.dart';
+
+/// Redirect configuration for 3D Secure.
+/// Register in [PAY.JP dashboard](https://pay.jp/d/settings).
+class PayjpThreeDSecureRedirect {
+  final String url;
+  final String key;
+  PayjpThreeDSecureRedirect({@required this.url, @required this.key});
+}
